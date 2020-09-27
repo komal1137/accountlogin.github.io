23f8be5de5847af3edc833ac54ac7fdf749399e7
komal kumari <kumarikomal14112002@gmail.com>
2020-09-27 09:22:54 +0530

    Create login.php
    This is last assignment for my php web development course in coursera

---
+++ login.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+<head>
+    <title>komal kumari</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    </head>
+    <body>
+     <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <h2>Login form</h2>
+             <form action="validation.php" method="POST">
+                <div class="form-group">
+                 <label>user name</label>
+                 <input type="text" name="user" class="form-control">
+                 </div>
+                 <div class="form-group">
+                
+                   <label>password</label>
+                    <input type="password" name="password" class="form-control>"
+                 </div>   
+                 <button class="btn btn-primary">Login</button>                            
+        
+             
+             </form>
+            </div> 
+            <div class="col-ig-6">
+            <h2>Signin form</h2>
+            <form action="registration.php" method="POST">
+               <div class="form-group">
+                 <label>user name</label>
+                 <input type="text" name="user" class="form-control">
+                 </div>
+                 <div class="form-group">
+                
+                   <label>password</label>
+                    <input type="password" name="password" class="form-control>"
+                 </div>   
+                 <button class="btn btn-primary">Signin</button>                            
+        
+             
+              
+                
+                
+                
+                
+                
+                
+                </form>    
+              
+              </div>     
+        </div>  
+    </div>      
+            
+            
+             
+             
+            
+            
+            
+    
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
